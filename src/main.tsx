5cc2be517b9536c9c78f26eba1426fa1f8708dfb
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-24 18:11:00 +0200

    styles: added animated wrapper and animation functions for Framer Motion library animations

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { TaskProvider } from "./lib/context/Tasks.tsx";
+import AnimationWrapper from "./components/AnimationWrapper.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TaskProvider>
-      <App />
+      <AnimationWrapper>
+        <App />
+      </AnimationWrapper>
     </TaskProvider>
   </React.StrictMode>
 );
