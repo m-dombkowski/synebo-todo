0bd4ad1fc40e3b5837074755f3707a39569dce96
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-24 21:52:04 +0200

    refractor: refractoring App component with, putting 2 new components into div with one old in the bottom of a page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import TaskList from "./components/TaskList";
 import TopNav from "./components/TopNav";
 import { TaskContext } from "./lib/context/Tasks";
 import BottomFilters from "./components/BottomFilters";
+import TaskCount from "./components/TaskCount";
+import ClearCompletedTasks from "./components/ClearCompletedTasks";
 
 function App() {
   const context = useContext(TaskContext);
@@ -13,13 +15,19 @@ function App() {
   const { tasks, filteredTasks } = context;
 
   return (
-    <div className="max-w-[1440px] w-full flex justify-center items-center m-auto">
+    <div className="max-w-[1440px] w-full flex justify-center items-center m-auto font-josefinSans">
       <Header />
       <div className="md:w-[550px] flex justify-center mt-20 items-center flex-col">
         <TopNav />
         <AddTaskForm />
         {tasks && <TaskList taskList={tasks} filteredTasks={filteredTasks} />}
-        <BottomFilters />
+        {tasks.length > 0 && (
+          <div className="justify-between items-center flex py-5 w-full px-5 shadow-2xl">
+            <TaskCount tasks={tasks} />
+            <BottomFilters />
+            <ClearCompletedTasks tasks={tasks} />
+          </div>
+        )}
       </div>
     </div>
   );
