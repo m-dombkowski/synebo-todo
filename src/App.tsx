97663e391a891080cacf0f53782e546590e9e0d8
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-24 18:18:29 +0200

    refractor: changing to conditional rendering for TaskList component. since it's rendering only when there are some tasks added and now typescript won't scream at me that "tasks" could be undefined

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import Header from "./components/Header";
 import TaskList from "./components/TaskList";
 import TopNav from "./components/TopNav";
 import { TaskContext } from "./lib/context/Tasks";
+import BottomFilters from "./components/BottomFilters";
 
 function App() {
   const context = useContext(TaskContext);
-  const taskList = context?.tasks;
+
+  if (!context) return;
+  const { tasks, filteredTasks } = context;
 
   return (
     <div className="max-w-[1440px] w-full flex justify-center items-center m-auto">
@@ -15,7 +18,8 @@ function App() {
       <div className="md:w-[550px] flex justify-center mt-20 items-center flex-col">
         <TopNav />
         <AddTaskForm />
-        <TaskList taskList={taskList} />
+        {tasks && <TaskList taskList={tasks} filteredTasks={filteredTasks} />}
+        <BottomFilters />
       </div>
     </div>
   );
