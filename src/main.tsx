f962ac24cecf92d329767deb33c1b8c15122a25a
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-24 21:54:09 +0200

    docs: removed react.stricteMode because it inteferes with DnD and brake it in development, attaching github issue post for clarification https://github.com/atlassian/react-beautiful-dnd/issues/2396

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import { TaskProvider } from "./lib/context/Tasks.tsx";
 import AnimationWrapper from "./components/AnimationWrapper.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <TaskProvider>
-      <AnimationWrapper>
-        <App />
-      </AnimationWrapper>
-    </TaskProvider>
-  </React.StrictMode>
+  <TaskProvider>
+    <AnimationWrapper>
+      <App />
+    </AnimationWrapper>
+  </TaskProvider>
 );
