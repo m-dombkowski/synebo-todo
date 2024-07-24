9cd5e652979f3be6e2a4dd385c757054ed0bb0b5
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-24 21:49:59 +0200

    feat: added TaskCount components which counts how many active tasks are left.

---
+++ src/components/TaskCount.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { TaskType } from "../lib/context/Tasks";
+
+export default function TaskCount({ tasks }: { tasks: TaskType[] }) {
+  const [activeTasks, setActiveTasks] = useState<number>(0);
+
+  useEffect(() => {
+    const filteredTasks = tasks.filter((task) => !task.completed);
+    setActiveTasks(filteredTasks.length);
+  }, [tasks]);
+
+  return (
+    <span className=" text-l-dark-grayish-blue">{activeTasks} items left</span>
+  );
+}
