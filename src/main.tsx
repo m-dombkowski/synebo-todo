c2100b1225d121e6ddb2cbbd50eac794b4fa9925
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-07-25 16:21:02 +0200

    docs: cleanup, removed unsed Wrapper and added Theme Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { TaskProvider } from "./lib/context/Tasks.tsx";
-import AnimationWrapper from "./components/AnimationWrapper.tsx";
+import { MyThemeContextProvider } from "./lib/context/Theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TaskProvider>
-    <AnimationWrapper>
+    <MyThemeContextProvider>
       <App />
-    </AnimationWrapper>
+    </MyThemeContextProvider>
   </TaskProvider>
 );
